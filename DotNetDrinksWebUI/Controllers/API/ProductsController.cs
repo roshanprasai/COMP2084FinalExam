5caefe996bf03f6db128de4044ba9c7e43451be4
roshanprasai <200598344@student.georgianc.on.ca>
2025-08-12 19:36:42 -0400

    added new controller class

---
+++ DotNetDrinksWebUI/Controllers/API/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DotNetDrinks.Data;  
+using DotNetDrinks.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DotNetDrinksWebUI.Models;
+using DotNetDrinksWebUI.Data;
+
+namespace DotNetDrinks.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
